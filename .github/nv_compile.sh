312f3c9d9b23d6663a64d5d80bf029ecd591a577
sappho <sappho@sappho.io>
2022-08-18 17:46:29 -0400

    fixup an oopsie with .sp.smx files

--- .github/nv_compile.sh
+++ .github/nv_compile.sh
@@ -5,18 +5,17 @@ versvers=$(spcomp64 -v | grep "Compiler" --color=never | cut -d " " -f 3)
 
 
 nv_plugs=("nativevotes")
-nv_plugs+=("nativevotes.sp")
-nv_plugs+=("nativevotes-basecommands.sp")
-nv_plugs+=("nativevotes_mapchooser.sp")
-nv_plugs+=("nativevotes_nominations.sp")
-nv_plugs+=("nativevotes_rockthevote.sp")
+nv_plugs+=("nativevotes-basecommands")
+nv_plugs+=("nativevotes_mapchooser")
+nv_plugs+=("nativevotes_nominations")
+nv_plugs+=("nativevotes_rockthevote")
 
-nv_plugins_disabled=("nativevotes_votetest.sp")
-nv_plugins_disabled+=("nativevotes_votemanager_test.sp")
-nv_plugins_disabled+=("csgo_votestart_test.sp")
-nv_plugins_disabled+=("votedelay_changelevel.sp")
-nv_plugins_disabled+=("votediagnostics.sp")
-nv_plugins_disabled+=("votefailed.sp")
+nv_plugins_disabled=("nativevotes_votetest")
+nv_plugins_disabled+=("nativevotes_votemanager_test")
+nv_plugins_disabled+=("csgo_votestart_test")
+nv_plugins_disabled+=("votedelay_changelevel")
+nv_plugins_disabled+=("votediagnostics")
+nv_plugins_disabled+=("votefailed")
 
 # we start in git root
 pushd ./addons/sourcemod/scripting
@@ -25,11 +24,11 @@ mkdir ../plugins/disabled -p || true
 
 
 for target in "${nv_plugs[@]}"; do
-    spcomp64 -i"./include/" "${target}" -o ../plugins/"${target}".smx || exit 1
+    spcomp64 -i"./include/" "${target}".sp -o ../plugins/"${target}".smx || exit 1
 done
 
 for target in "${nv_plugins_disabled[@]}"; do
-    spcomp64 -i"./include/" "${target}" -o ../plugins/disabled/"${target}".smx || exit 1
+    spcomp64 -i"./include/" "${target}".sp -o ../plugins/disabled/"${target}".smx || exit 1
 done
 
 popd
